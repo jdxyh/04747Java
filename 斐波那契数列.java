aae507b59f5b7e3b77059df96976308789c964ee
jdxyh <763998827@qq.com>
2019-07-16 15:17:44 +0800

    Update and rename 20190711.java to 斐波那契数列.java

--- 斐波那契数列.java
+++ 斐波那契数列.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 /**
  * 斐波那契数列应用
  * 计算应用要点f(n)=f(n-1)+f(n-2)
  * */
-class test1 {
+class 斐波那契数列 {
 	public static void main(String[] args) {
-		System.out.println("请输入第几个数，以回车结束：");
-		Scanner sc = new Scanner(Sytem.in);
+		System.out.println("请输入要求斐波那契数列的长度，以回车结束：");
+		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
-		for(int i = 1;i <= x; i++){
+		for(int i = 1;i <= x; i++)
+		{
 			System.out.println(fib(i)+"\t");
 		}
 	}
@@ -15,7 +18,7 @@ class test1 {
 	public static int fib(int num){
 		//外部传入一个整数，进行判断，如若是第1，2个，则返回1，否则返回fib(num-2)+fib(num-1)
 	if(num == 1|| num == 2){
-		return 1；
+		return 1;
 	}
 	else{
 		return fib(num-2)+fib(num-1);
